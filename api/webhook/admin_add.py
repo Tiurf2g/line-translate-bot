12d942e61a26cb9504b42d5816850efd1a869ee8
Tiurf2g <96918452+Tiurf2g@users.noreply.github.com>
2025-12-16 15:46:12 +0800

    Delete api/webhook directory

--- api/webhook/admin_add.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, Request, Form
-from webhook import admin_add as _admin_add
-
-app = FastAPI()
-
-@app.post("/")
-def add(request: Request, bucket: str = Form(...), k: str = Form(...), v: str = Form(...)):
-    return _admin_add(request, bucket=bucket, k=k, v=v)
--- api/webhook/admin_del.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, Request, Form
-from webhook import admin_del as _admin_del
-
-app = FastAPI()
-
-@app.post("/")
-def delete(request: Request, bucket: str = Form(...), k: str = Form(...)):
-    return _admin_del(request, bucket=bucket, k=k)
